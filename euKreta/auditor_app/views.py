4fb052a83479bbe459881bac0512f6304cba55fa
Nikhil <thakrenikhil45@gmail.com>
2023-11-10 05:57:21 +0530

    before ppt

--- euKreta/auditor_app/views.py
+++ euKreta/auditor_app/views.py
@@ -9,6 +9,9 @@ from pydub import AudioSegment
 from moviepy.editor import AudioFileClip
 import speech_recognition as sr
 from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+from keybert import KeyBERT
+import googletrans
+from googletrans import Translator
 
 def index(request):
     context ={
@@ -22,7 +25,7 @@ def Document_save(request):
         audio = request.FILES["audio"]
         code = datetime.datetime.now()
         index = 0
-        audio_name = []
+        objectArr = []
         for audio in request.FILES.getlist('audio'):
             audio_file = original_audio.objects.create(
                                         name=audio.name,
@@ -30,17 +33,43 @@ def Document_save(request):
                                         code = code
                                         )
             # audio_name[index] = audio_file.name
-            audio_name.insert(index, audio_file.name)
+            transcript, emotion_labels,keywords=create_transcript_emotion(audio)
+            tempKey = []
+            i=0
+            for item in keywords:
+                temp = { item[0] , item[1]}
+                tempKey.insert(i, temp)
+                i = i +1
+            print(tempKey)
+            object = {
+                "audio_name" : audio_file.name,
+                "transcript":transcript,
+                "emotion_labels":emotion_labels,
+                "keywords":tempKey
+            }
+            objectArr.insert(index, object)
             index = index +1
             
-            transcript, emotion_labels=create_transcript_emotion(audio)
             print(transcript,"hello Nikhil ")
         print(code)
-        context= {"audio_name":audio_name , "code":str(code) , "transcript":transcript,"emotion_labels":emotion_labels
+        context= {"processed":objectArr , "code":str(code) , "transcript":transcript,"emotion_labels":emotion_labels,"keywords":keywords
                   }
         return render(request, "upload.html", context)
     return render(request, "index.html")
        
+def Keywords_ex(text):
+    kw_model = KeyBERT()
+    keywords =  kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 2), stop_words=None)
+    
+    return keywords
+
+def translater(text):
+    translator=Translator()
+    lang_det=translator.detect(text)
+    
+    changed_txt= translator.translate(text, src='en', dest='tr')
+    return lang_det,changed_txt
+
 
 def processed_data(request,pk) :
     processed_data= Processed.objects.get()
@@ -108,6 +137,7 @@ def aud2txt(generated_wav):
 def create_transcript_emotion(audio):
     print(f"Input audio: {audio}")
     input_mp3 = 'http://127.0.0.1:8000/media/' + audio.name
+
     output_wav =audio.name + "_output.wav"
     
     try:
@@ -125,18 +155,36 @@ def create_transcript_emotion(audio):
             snippet_transcript = aud2txt(input_wav)
             if snippet_transcript:
                 transcript += snippet_transcript + " "
-        
+     
+       
         print("\nFinal Transcript:")
         print(transcript)
-        
+        keywords=Keywords_ex(transcript)
+        print(Keywords_ex(transcript))
+
+
         RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
         TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 
 
         emotion = pipeline('sentiment-analysis',model='arpanghoshal/EmoRoBERTa')
-        emotion_labels = emotion(transcript)
+        emotion_det= emotion(transcript)
+        print(emotion_det)
+        emotion_detected=emotion_det[0]["label"].capitalize()
+        print("Ye lo",emotion_detected)
+        satis=["Admiration","Amusement","Approval","Caring","Desire","Excitement","Gratitude","Joy","Love","Optimism","Pride","Realization","Relief","Surprise"]
+        neutral=["Confusion","Curiosity","Neutral"]
+        unsatis=["Anger","Disappointment","Disapproval","Disgust","Embarrassment","Fear","Grief","Nervousness","Remorse","Sadness"]
+        if emotion_detected in satis :
+            emotion_labels="Satisfied"
+        elif emotion_detected in neutral:
+            emotion_labels="Neutral"
+        else:
+            emotion_labels="Dissatisfied" 
+
         print(emotion_labels,"emotion label in function")
-        return transcript ,emotion_labels
+
+        return transcript ,emotion_labels,keywords
     except Exception as e:
         print(f"An error occurred: {e}")
 
